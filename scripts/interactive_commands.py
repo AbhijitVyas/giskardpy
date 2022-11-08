cb1371e0ab134a5debe549c3e1103e29df27c00c
Abhijit Vyas <vyasabhijit21@gmail.com>
2022-11-08 16:07:57 +0100

    Rest interface added to giskard interactive commands. So that now you can call the methods form web browser.

--- scripts/interactive_commands.py
+++ scripts/interactive_commands.py
@@ -10,10 +10,44 @@ rospy.init_node('test')
 rospy.loginfo('Instantiating Giskard wrapper.')
 giskard_wrapper = GiskardWrapper()
 
+# create goal joint state dictionary
+start_joint_state = {'r_elbow_flex_joint': -1.29610152504,
+                     'r_forearm_roll_joint': -0.0301682323805,
+                     'r_shoulder_lift_joint': 1.20324921318,
+                     'r_shoulder_pan_joint': -0.73456435706,
+                     'r_upper_arm_roll_joint': -0.70790051778,
+                     'r_wrist_flex_joint': -0.10001,
+                     'r_wrist_roll_joint': 0.258268529825,
 
-def clear_world():
+                     'l_elbow_flex_joint': -1.29610152504,
+                     'l_forearm_roll_joint': 0.0301682323805,
+                     'l_shoulder_lift_joint': 1.20324921318,
+                     'l_shoulder_pan_joint': 0.73456435706,
+                     'l_upper_arm_roll_joint': 0.70790051778,
+                     'l_wrist_flex_joint': -0.1001,
+                     'l_wrist_roll_joint': -0.258268529825,
+
+                     'torso_lift_joint': 0.2,
+                     'head_pan_joint': 0,
+                     'head_tilt_joint': 0}
+
+
+def reset():
     # Remove everything but the robot.
     giskard_wrapper.clear_world()
+    base_goal = PoseStamped()
+    base_goal.header.frame_id = 'map'
+    base_goal.pose.position = Point(0, 0, 0)
+    base_goal.pose.orientation = Quaternion(0, 0, 0, 1)
+    # Setting the Cartesian goal.
+    # Choosing map as root_link will allow Giskard to drive with the pr2.
+    rospy.loginfo('Reset the pr2 to origin.')
+    giskard_wrapper.set_cart_goal(root_link='map', tip_link='base_footprint', goal_pose=base_goal)
+    # Setting the joint goal
+    giskard_wrapper.set_joint_goal(start_joint_state)
+    # Turn off collision avoidance to make sure that the robot can recover from any state.
+    giskard_wrapper.allow_all_collisions()
+    return giskard_wrapper.plan_and_execute()
 
 
 # Method prompts agent to move to given pose.
@@ -29,7 +63,7 @@ def move(pose):
     giskard_wrapper.set_cart_goal(root_link='map', tip_link='base_footprint', goal_pose=base_goal)
     # Turn off collision avoidance to make sure that the robot can recover from any state.
     giskard_wrapper.allow_all_collisions()
-    giskard_wrapper.plan_and_execute()
+    return giskard_wrapper.plan_and_execute()
 
 
 # Method prompts agent to move to given pose.
@@ -46,7 +80,7 @@ def move_forward():
     giskard_wrapper.set_cart_goal(root_link='map', tip_link='base_footprint', goal_pose=base_goal)
     # Turn off collision avoidance to make sure that the robot can recover from any state.
     giskard_wrapper.allow_all_collisions()
-    giskard_wrapper.plan_and_execute()
+    return giskard_wrapper.plan_and_execute()
 
 
 # Method prompts agent to move to given pose.
@@ -63,7 +97,7 @@ def move_backward():
     giskard_wrapper.set_cart_goal(root_link='map', tip_link='base_footprint', goal_pose=base_goal)
     # Turn off collision avoidance to make sure that the robot can recover from any state.
     giskard_wrapper.allow_all_collisions()
-    giskard_wrapper.plan_and_execute()
+    return giskard_wrapper.plan_and_execute()
 
 
 # Method prompts agent to move to given pose.
@@ -80,7 +114,7 @@ def move_left():
     giskard_wrapper.set_cart_goal(root_link='map', tip_link='base_footprint', goal_pose=base_goal)
     # Turn off collision avoidance to make sure that the robot can recover from any state.
     giskard_wrapper.allow_all_collisions()
-    giskard_wrapper.plan_and_execute()
+    return giskard_wrapper.plan_and_execute()
 
 
 # Method prompts agent to move to given pose.
@@ -97,4 +131,67 @@ def move_right():
     giskard_wrapper.set_cart_goal(root_link='map', tip_link='base_footprint', goal_pose=base_goal)
     # Turn off collision avoidance to make sure that the robot can recover from any state.
     giskard_wrapper.allow_all_collisions()
-    giskard_wrapper.plan_and_execute()
+    return giskard_wrapper.plan_and_execute()
+
+
+# Method prompts agent to raise right hand.
+# call this method from commandline with following command:
+# python3 -c 'from interactive_commands import *; raise_right_hand()'
+def raise_right_hand():
+    rospy.loginfo('Move right hand upward in +z direction.')
+    base_goal = PoseStamped()
+    # it is very important to use goal.header.frame_id as r_gripper_tool_frame 
+    # so that the movements will be relative to the current base positions
+    base_goal.header.frame_id = 'r_gripper_tool_frame'
+    base_goal.pose.position.z = +0.3
+    rospy.loginfo('Setting Cartesian goal.')
+    giskard_wrapper.set_cart_goal(root_link='base_footprint', tip_link='r_gripper_tool_frame', goal_pose=base_goal)
+    # Turn off collision avoidance to make sure that the robot can recover from any state.
+    giskard_wrapper.allow_all_collisions()
+    return giskard_wrapper.plan_and_execute()
+
+
+# Method prompts agent to raise left hand.
+# call this method from commandline with following command:
+# python3 -c 'from interactive_commands import *; raise_left_hand()'
+def raise_left_hand():
+    rospy.loginfo('Move left hand upward in +z direction.')
+    base_goal = PoseStamped()
+    # it is very important to use goal.header.frame_id as base_footprint 
+    # so that the movements will be relative to the current base positions
+    base_goal.header.frame_id = 'l_gripper_tool_frame'
+    base_goal.pose.position.z = +0.3
+    rospy.loginfo('Setting Cartesian goal.')
+    giskard_wrapper.set_cart_goal(root_link='base_footprint', tip_link='l_gripper_tool_frame', goal_pose=base_goal)
+    # Turn off collision avoidance to make sure that the robot can recover from any state.
+    giskard_wrapper.allow_all_collisions()
+    return giskard_wrapper.plan_and_execute()
+
+# TODO: method: grasp_object_with_right_hand(pose):
+# this method should make pr2 to grasp the object with right hand at give position
+
+
+def follow_human_right_hand(pose1, pose2, ori2):
+# this method should make pr2 to follow right hand of the human from VR demonstrations.
+# here first you calculate the difference between two points in human hand trajectory, such as diff = point2 - point1, in all 3 directions as well  as rotations
+# then you can tranlate this difference into pr2's relative hand motions as follows:
+
+    rospy.loginfo('Move right hand according to human hand.')
+    base_goal = PoseStamped()
+    # it is very important to use goal.header.frame_id as base_footprint 
+    # so that the movements will be relative to the current base positions
+    base_goal.header.frame_id = 'r_gripper_tool_frame'
+    base_goal.pose.position.x = pose2.x - pose1.x
+    base_goal.pose.position.y = pose2.y - pose1.y
+    base_goal.pose.position.z = pose2.z - pose1.z
+
+    base_goal.pose.orientation = ori2
+
+    rospy.loginfo('Setting Cartesian goal.')
+    giskard_wrapper.set_cart_goal(root_link='base_footprint', tip_link='r_gripper_tool_frame', goal_pose=base_goal)
+    # Turn off collision avoidance to make sure that the robot can recover from any state.
+    giskard_wrapper.allow_all_collisions()
+    return giskard_wrapper.plan_and_execute()
+
+
+
---
+++ scripts/rest_client.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import os
+from flask import Flask, jsonify
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+from geometry_msgs.msg import Point, Quaternion
+import interactive_commands as ic
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route("/")
+def get_hello_world():
+    return "Hello, World!"
+
+
+@app.route("/move_forward")
+def move_pr2_forward():
+    response = ic.move_forward()
+    if response is not None:
+        return jsonify("successfully moved forward"), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/move_backward")
+def move_pr2_backward():
+    response = ic.move_backward()
+    if response is not None:
+        return jsonify("successfully moved backward"), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/follow_human_right_hand")
+def follow_human_right_hand():
+    pose1 = Point(2.0,2.0,1.0)
+    pose2 = Point(2.1,2.1,1.1)
+    # TODO: next step here is to find all the trajectory points that human hand has followed while doping pouring. 
+    #  or atlest at start of each events. Then pass that list of poses. In the metod inside interactive_commands.py you 
+    #  can calculate the difference for each subsequent point. And then giskard_wrapper.add_cmd() to giskard so 
+    #  that the difference between those points is saved into giskard and let giskard calulate trajectory for pr2 hands.
+    ori2 = Quaternion(0, 0, 0, 1)
+    response = ic.follow_human_right_hand(pose1, pose2, ori2)
+    if response is not None:
+        return jsonify("successfully moved backward"), 200
+    else:
+        return jsonify(response), 400
